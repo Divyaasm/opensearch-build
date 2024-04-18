eec2b7f71337ecc97a8656227a3c967417320478
Divya Madala <divyaasm@amazon.com>
2024-04-18 15:40:34 -0700

    Test executions
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/test_workflow/benchmark_test/benchmark_test_suite.py
+++ src/test_workflow/benchmark_test/benchmark_test_suite.py
@@ -78,6 +78,7 @@ class BenchmarkTestSuite:
         subprocess.check_call(f"{self.command}", cwd=os.getcwd(), shell=True)
         subprocess.check_call(f"docker start contain", cwd=os.getcwd(), shell=True)
         path = subprocess.check_output("docker exec contain find /opensearch-benchmark -name test_execution.json", cwd=os.getcwd(), shell=True)
+        logging.info(path)
         tmp_dir = TemporaryDirectory()
         subprocess.check_call(f"docker cp contain:{path.decode().strip()} {str(tmp_dir.path)}", cwd=os.getcwd(), shell=True)
         subprocess.check_call(f"docker stop contain", cwd=os.getcwd(), shell=True)
