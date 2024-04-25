324bbe129bcd377ced41ae9dce09dae5639f10da
Divya Madala <divyaasm@amazon.com>
2024-04-25 10:45:33 -0700

    Add changes
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/test_workflow/benchmark_test/benchmark_test_suite.py
+++ src/test_workflow/benchmark_test/benchmark_test_suite.py
@@ -13,6 +13,7 @@ import subprocess
 from typing import Any
 
 from test_workflow.benchmark_test.benchmark_args import BenchmarkArgs
+from system.temporary_directory import TemporaryDirectory
 
 
 class BenchmarkTestSuite:
@@ -26,6 +27,9 @@ class BenchmarkTestSuite:
     Represents a performance test suite. This class runs rally test on the deployed cluster with the provided IP.
     """
 
+    def __exit__(self, exc_type: Any, exc_value: Any, exc_traceback: Any) -> None:
+        self.tmp_dir.__exit__(exc_type, exc_value, exc_traceback)
+
     def __init__(
             self,
             endpoint: Any,
@@ -37,11 +41,13 @@ class BenchmarkTestSuite:
         self.security = security
         self.args = args
         self.password = password
+        self.tmp_dir = TemporaryDirectory(keep=keep)
 
         # Pass the cluster endpoints with -t for multi-cluster use cases(e.g. cross-cluster-replication)
-        self.command = 'docker run --name contain'
+        self.container_name = f'docker-container-{args.stack_suffix}'  # container name
+        self.command = f'docker run --name {self.container_name}'
         if self.args.benchmark_config:
-            self.command += f" -v {args.benchmark_config}:/opensearch-benchmark/.benchmark/benchmark.ini"
+            self.command += f" -it -v {args.benchmark_config}:/opensearch-benchmark/.benchmark/benchmark.ini"
         self.command += f" opensearchproject/opensearch-benchmark:latest execute-test --workload={self.args.workload} " \
                         f"--pipeline=benchmark-only --target-hosts={endpoint} --test-mode"
 
@@ -78,19 +84,18 @@ class BenchmarkTestSuite:
         logging.info(log_info.replace(self.password, len(self.password) * '*') if self.password else log_info)
         subprocess.check_call(f"{self.command}", cwd=os.getcwd(), shell=True)
 
-        subprocess.check_call("docker start contain", cwd=os.getcwd(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        path = subprocess.check_output("docker exec contain find /opensearch-benchmark -name test_execution.json", cwd=os.getcwd(), shell=True)
-        subprocess.check_call(f"docker cp contain:{path.decode().strip()} .", cwd=os.getcwd(), shell=True)
+        subprocess.check_call(f"docker start {self.container_name}", cwd=os.getcwd(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        path = subprocess.check_output(f"docker exec {self.container_name} find /opensearch-benchmark -name test_execution.json", cwd=os.getcwd(), shell=True)
+        subprocess.check_call(f"docker cp {self.container_name}:{path.decode().strip()} .", cwd=os.getcwd(), shell=True)
         file_path = os.path.join(os.getcwd(), "test_execution.json")
         self.convert(file_path)
-        subprocess.check_call("docker stop contain", cwd=os.getcwd(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        subprocess.check_call("docker rm contain", cwd=os.getcwd(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.check_call(f"docker stop {self.container_name}", cwd=os.getcwd(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.check_call(f"docker rm {self.container_name}", cwd=os.getcwd(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     def convert(self, results: str) -> None:
         with open(results) as file:
             data = json.load(file)
         formatted_data = pd.json_normalize(data["results"]["op_metrics"])
         formatted_data.to_csv("test_execution.csv", index=False)
-
-
-
+        df = pd.read_csv(os.path.join(os.getcwd(), "test_execution.csv"))
+        logging.info(df)
