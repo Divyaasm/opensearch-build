5fbea63f9ec5826313fa439fe7d9317191b982ec
Divya Madala <divyaasm@amazon.com>
2024-06-27 09:47:43 -0700

    Archive .json
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/test_workflow/benchmark_test/benchmark_test_suite.py
+++ src/test_workflow/benchmark_test/benchmark_test_suite.py
@@ -47,7 +47,7 @@ class BenchmarkTestSuite:
         if self.args.benchmark_config:
             self.command += f" -v {args.benchmark_config}:/opensearch-benchmark/.benchmark/benchmark.ini"
         self.command += f" opensearchproject/opensearch-benchmark:latest execute-test --workload={self.args.workload} " \
-                        f"--pipeline=benchmark-only --target-hosts={endpoint}"
+                        f"--pipeline=benchmark-only --target-hosts={endpoint} --test-mode"
 
         if self.args.workload_params:
             logging.info(f"Workload Params are {args.workload_params}")
