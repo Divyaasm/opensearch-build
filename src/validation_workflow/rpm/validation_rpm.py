d97ed46c3aa4a2326c6fa9c621ebd27a2f325ca6
Divya Madala <divyaasm@amazon.com>
2024-09-06 17:53:41 -0700

    Test rpm metadata changes
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/validation_workflow/rpm/validation_rpm.py
+++ src/validation_workflow/rpm/validation_rpm.py
@@ -115,18 +115,15 @@ class ValidateRpm(Validation, DownloadUtils):
         key_list = ["Header V4 RSA/SHA512 Signature, key ID 9310d3fc", "Header SHA256 digest", "Header SHA1 digest", "Payload SHA256 digest", "V4 RSA/SHA512 Signature, key ID 9310d3fc", "MD5 digest"]
         present_key = []
         x = 0
-        for line in stdout.split('\n'):
+        for line in stdout.split('\n').strip():
             print(x)
             x=x+1
             key = line.split(':')[0].strip()
             print(key)
-            if (key == os.path.join(self.tmp_dir.path, self.filename)):
-                continue
-            else:
-                var = line.split(':')[1].strip()
-                logging.info(f"var:{var}")
-                logging.info(f"{key} is validated as: {line}")
-                present_key.append(key)
+            if key != os.path.join(self.tmp_dir.path, self.filename):
+                if len(line.split(':', 1)) > 1 and "OK" == line.split(':')[1].strip():
+                    logging.info(f"{key} is validated as: {line}")
+                    present_key.append(key)
         logging.info("Validation of all key digests starts: ")
         for digest in key_list:
             if digest in present_key:
