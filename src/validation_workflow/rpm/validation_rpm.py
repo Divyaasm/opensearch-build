1e4e396935e51324061442093590d06d2cdeaee6
Divya Madala <divyaasm@amazon.com>
2024-09-06 17:26:08 -0700

    Test rpm metadata changes
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/validation_workflow/rpm/validation_rpm.py
+++ src/validation_workflow/rpm/validation_rpm.py
@@ -119,7 +119,7 @@ class ValidateRpm(Validation, DownloadUtils):
             print(key)
 #            #if (key == os.path.join(self.tmp_dir.path, self.filename)):
 #            #     continue
-            if "OK" == line.split(':', 1)[1].strip():
+            if "OK" == line.split(':')[1].strip():
                 logging.info(f"{key} is validated as: {line}")
             present_key.append(key)
         logging.info("Validation of all key digests starts: ")
