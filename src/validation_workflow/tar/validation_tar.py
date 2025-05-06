a7baf4f0fec1b879dbc70797263beae4c5ab3b82
Divya Madala <divyaasm@amazon.com>
2025-05-05 17:12:21 -0700

    Check native plugins
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/validation_workflow/tar/validation_tar.py
+++ src/validation_workflow/tar/validation_tar.py
@@ -31,7 +31,7 @@ class ValidateTar(Validation, DownloadUtils):
                 self.filename = os.path.basename(self.args.file_path.get(project))
                 execute('mkdir ' + os.path.join(self.tmp_dir.path, project) + ' | tar -xzf ' + os.path.join(str(self.tmp_dir.path), self.filename) + ' -C ' + os.path.join(self.tmp_dir.path, project) + ' --strip-components=1', ".", True, False)  # noqa: E501
                 (returncode, stdout, stderr) = execute('ls', os.path.join(self.tmp_dir.path, project))
-                logging.info(stderr, stdout)
+                logging.info(stdout)
         except:
             raise Exception('Failed to install Opensearch')
         return True
