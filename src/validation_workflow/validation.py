6d93479ae7a6890372e944827f49ee61856c3fbd
Divya Madala <divyaasm@amazon.com>
2025-06-23 14:23:28 -0700

    Fix issues
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/validation_workflow/validation.py
+++ src/validation_workflow/validation.py
@@ -75,7 +75,7 @@ class Validation(ABC):
                     f'ls {native_plugin}-{self.args.version}.zip',
                     cwd=os.path.join(path, "bin"),
                     shell=True, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-                logging.info(result)
+                logging.info('.' + os.sep + f'opensearch-plugin install --batch file:{os.path.join(path, "bin",{native_plugin}-{self.args.version}.zip)}')
 
                 result_inspect = subprocess.run('.' + os.sep + f'opensearch-plugin install --batch file:{os.path.join(path, "bin",{native_plugin}-{self.args.version}.zip)}', cwd=os.path.join(path, "bin"),
                                                 shell=True, stdout=PIPE, stderr=PIPE, universal_newlines=True)
