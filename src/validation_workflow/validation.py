ae3f393a782cabf665093003ccc7cfbb1e5490fb
Divya Madala <divyaasm@amazon.com>
2025-06-23 11:40:10 -0700

    Fix issues
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/validation_workflow/validation.py
+++ src/validation_workflow/validation.py
@@ -66,11 +66,15 @@ class Validation(ABC):
                              f'{self.args.arch}/{self.args.distribution}/builds/opensearch/core-plugins/{native_plugin}-{self.args.version}.zip'
                 try:
                     logging.info(plugin_url)
-                    urllib.request.urlretrieve(plugin_url, path)
+                    urllib.request.urlretrieve(plugin_url, os.path.join(path, f'{native_plugin}-{self.args.version}.zip'))
                 except Exception as e:
                     logging.info(e)
+                result = subprocess.run(
+                    'ls',
+                    cwd=os.path.join(path),
+                    shell=True, stdout=PIPE, stderr=PIPE, universal_newlines=True)
 
-                result_inspect = subprocess.run('.' + os.sep + f'opensearch-plugin install --batch {os.path.join(path, "bin")}/repository-s3-2.19.1.zip', cwd=os.path.join(path, "bin"),
+                result_inspect = subprocess.run('.' + os.sep + f'opensearch-plugin install --batch {os.path.join(path)}/{native_plugin}-{self.args.version}.zip', cwd=os.path.join(path, "bin"),
                                                 shell=True, stdout=PIPE, stderr=PIPE, universal_newlines=True)
                 logging.info(result_inspect)
 
