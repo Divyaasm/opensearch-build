0f27fdd23fb0dff1a39159803d1d33d112f03b06
Divya Madala <divyaasm@amazon.com>
2026-01-15 11:55:08 -0800

    test plugin validation
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/validation_workflow/validation.py
+++ src/validation_workflow/validation.py
@@ -95,7 +95,7 @@ class Validation(ABC):
             plugin_list = [i["name"] for i in response if i["name"] not in installed_plugins_list]
             plugin_list.remove("examples")
             plugin_list.remove("build.gradle")
-            if Version(version) < Version("3.5.0"):
+            if Version(self.args.version) < Version("3.5.0"):
                 plugin_list.remove("identity-shiro")  # Since the security plugin is enabled in the artifacts and identity-shiro is also an identity plugin, we cannot have both the plugins installed together. # noqa: E501
             logging.info(plugin_list)
             return plugin_list
