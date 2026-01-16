f408e0173f4c97a024e7e12a2608ec0f4987f338
Divya Madala <divyaasm@amazon.com>
2026-01-16 11:49:45 -0800

    Added tests
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/validation_workflow/validation.py
+++ src/validation_workflow/validation.py
@@ -90,14 +90,15 @@ class Validation(ABC):
             plugin_url = f"https://api.github.com/repos/opensearch-project/OpenSearch/contents/plugins?ref={commit_id}"
             api_response = requests.get(plugin_url)
             logging.info(api_response)
-            api_response.raise_for_status()
+            if api_response.status_code != 200:
+                raise Exception("Github Api returned error code while retrieving the list of native plugins")
             response = api_response.json()
             logging.info(response)
             plugin_list = [i["name"] for i in response if i["name"] not in installed_plugins_list]
             plugin_list.remove("examples")
             plugin_list.remove("build.gradle")
-            # if Version(self.args.version) < Version("3.5.0"):
-            plugin_list.remove("identity-shiro")  # Since the security plugin is enabled in the artifacts and identity-shiro is also an identity plugin, we cannot have both the plugins installed together. # noqa: E501
+            if Version(self.args.version) < Version("3.5.0"):
+                plugin_list.remove("identity-shiro")  # Since the security plugin is enabled in the artifacts and identity-shiro is also an identity plugin, we cannot have both the plugins installed together. # noqa: E501
             logging.info(plugin_list)
             return plugin_list
         except Exception:
